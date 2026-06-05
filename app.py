c2d928d994b7f27e5465cdfef4e136f15f2603cd
caur4mos-dot <caur4mos@gmail.com>
2026-06-05 01:16:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,18 @@ na Bahia utilizando dados do SISMIGRA.
 - Visualização Espacial
 - Predição dos Fluxos Migratórios
 """)
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Migração Internacional na Bahia",
+    layout="wide"
+)
+
+st.title("Migração Internacional na Bahia (2021–2025)")
+
+st.markdown("""
+Este painel apresenta análises estatísticas,
+espaciais e preditivas dos registros migratórios
+na Bahia.
+""")
