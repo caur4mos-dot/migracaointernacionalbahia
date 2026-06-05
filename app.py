31798f144b418c8f2def2bd8f2de88ed9a3bcd0e
caur4mos-dot <caur4mos@gmail.com>
2026-06-05 12:39:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ st.title("Análise temporal, espacial e sociodemográfica da migração internac
 
 st.markdown("""
 Este site apresenta análises dos fluxos migratórios internacionais regularizados
-na Bahia utilizando dados do SISMIGRA.
+na Bahia utilizando dados do SISMIGRA com o objetivo de compreender os padrões migratórios e apoiar a gestão da Bahia no fortalecimento de políticas públicas de acolhimento,
+regularização documental, inclusão social, emprego, educação e planejamento territorial. Se alinhando a lei de Migração n° 13.445/2017 e ao Objetivo de Desenvolvimento Sustentável
+(ODS) 10.7, que prevê a facilitaçãode uma migração segura e regular, e o ODS 16, que enfatiza instituições eficazes e acesso à justiça.
+
 
 ### Seções
 
