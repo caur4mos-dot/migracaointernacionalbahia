dc705f7a3739199498396083d602ec560ef184fa
caur4mos-dot <caur4mos@gmail.com>
2026-06-06 01:40:01 -0300

    Delete pages/pages directory

--- pages/pages/2_Visualizacao_Espacial.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("Visualização Espacial")
-
-st.write("Página em construção.")
--- pages/pages/pages/3_Predicao.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("Predição")
-
-st.write("Página em construção.")
